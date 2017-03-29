101be60104c7e41dc419b7001a5295c3f0bd2706
Erin Nichols <erin.nichols@slate.com>
2017-03-29 10:24:29 -0400

    Return a promise

--- day-2/class-code/server.js
+++ day-2/class-code/server.js
@@ -8,21 +8,13 @@
 let TicTacToeGame = require('./src/tic-tac-toe-game');
 let fs = require('fs');
 let Promise = require('bluebird');
-
 let readDir = Promise.promisify(fs.readdir);
 let readFile = Promise.promisify(fs.readFile);
+const fileDir = './sandwich'
 
-let gameList = [];
-
-function processFile(fileName) {
-  return readFile(`./sandwich/${fileName}`, {encoding: 'utf-8'})
-    .then(json => TicTacToeGame.fromJSON(json))
-    .then(game => gameList.push(game));
-}
-
-readDir('./sandwich')
+module.exports = readDir(fileDir)
   // .map(stuff => console.log(stuff))
-  .map(fileName => processFile(fileName))
-  .then(() => console.log(gameList));
-
-module.exports = gameList;
+  .map(fileName => readFile(`${fileDir}/${fileName}`, {encoding: 'utf-8'}))
+  .map(json => TicTacToeGame.fromJSON(json))
+  .then(gameList => console.log(gameList))
+  .catch(err => console.error(err))
